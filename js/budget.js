65d2a7e8600d95aff430a7067db236391a680931
victoryagoc <victoryagoc@gmail.com>
2023-12-29 02:15:10 -0300

    great

--- js/budget.js
+++ js/budget.js
@@ -1,91 +1,218 @@
 // Function to open the shopping cart with customization options for a selected product
 function openCart(product, imageUrl) {
-    // Display the shopping cart
-    document.getElementById('cart').style.display = 'block';
-  
-    // Populate customization options in the cart
-    document.getElementById('customization-options').innerHTML = `
-      <p>Product: ${product}</p>
-      <img src="${imageUrl}" alt="${product}" style="max-width: 100px; height: auto; margin-bottom: 10px;">
-      <label>Malha da Peça:
-        <input type="text" name="material">
-      </label>
-      <br>
-      <label>Cor da Peça:
-        <input type="text" name="color">
-      </label>
-      <br>
-      <label>Tipo de Gola:
-        <input type="text" name="collar">
-      </label>
-      <br>
-      <label>Quantidade de Peças:
-        <input type="number" name="quantity" value="1" min="1">
-      </label>
-      <br>
-      <label>Tamanho da Peça (Max ${getMaxQuantity()}):
-        <input type="text" name="size">
-        <br>
-        <label>Quantidade por Tamanho:
-          <input type="number" name="sizeQuantity" value="1" min="1">
-        </label>
-      </label>
-      <br>
-      <label>Personalização (Bordado/Silkscreen):
-        <select name="personalization">
-          <option value="bordado">Bordado</option>
-          <option value="silkscreen">Silkscreen</option>
-        </select>
-      </label>
-      <br>
-      <label>Anexar Arquivo:
-        <input type="file" name="attachment">
-      </label>
-    `;
+  // Display the shopping cart
+  document.getElementById("cart").style.display = "block";
+
+  // Populate customization options in the cart based on the selected product
+  populateCustomizationOptions(product);
+
+  // Add product information to the cart
+  const productInfoContainer = document.getElementById("product-info");
+  productInfoContainer.innerHTML = `
+    <p>${product}</p>
+    <img src="${imageUrl}" alt="${product}" style="max-width: 100px; height: auto; margin-bottom: 10px;">
+  `;
+}
+
+// Function to close the shopping cart
+function closeCart() {
+  document.getElementById("cart").style.display = "none";
+}
+
+function saveCustomization() {
+  console.log("Save button clicked");
+
+  // Get selected product and image
+  const productInfoContainer = document.getElementById("product-info");
+  const product = productInfoContainer.querySelector("p").innerText.trim();
+  const imageUrl = productInfoContainer.querySelector("img").src;
+
+  console.log("Selected Product:", product);
+
+  // Get customization options
+  const customizationOptions = document.getElementById("customization-options");
+  const selectedOptions = [];
+
+  for (const element of customizationOptions.children) {
+    if (
+      element.tagName === "DIV" &&
+      element.classList.contains("customization-option")
+    ) {
+      const label = element.querySelector("label").innerText;
+      const input = element.querySelector("select, input");
+      const value = input ? input.value : null;
+
+      console.log("Label:", label);
+      console.log("Input:", input);
+      console.log("Value:", value);
+
+      selectedOptions.push({ label, value });
+    }
   }
-  
-  // Function to get the maximum quantity allowed for a product
-  function getMaxQuantity() {
-    // You can define the maximum quantity dynamically based on your requirements
-    return 10;
+
+  console.log("Selected Options:", selectedOptions);
+
+  // Create a list item to display saved options with the product image
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `
+    <div>
+      <img src="${imageUrl}" alt="${product}" style="max-width: 50px; height: auto; margin-right: 10px;">
+      <span>${selectedOptions
+        .map((option) => `${option.label} - ${option.value}`)
+        .join(", ")}</span>
+    </div>
+  `;
+
+  // Append the list item to the basket
+  document.getElementById("basket-list").appendChild(listItem);
+
+  // Close the cart after saving
+  closeCart();
+}
+
+// Function to populate customization options based on the selected item
+function populateCustomizationOptions(item) {
+  const customizationOptionsContainer = document.getElementById(
+    "customization-options"
+  );
+
+  // Clear existing content
+  customizationOptionsContainer.innerHTML = "";
+
+  const customizationOptions = {
+    camiseta: [
+      { label: "Malha da Peça", values: ["Algodão", "Poliviscose"] },
+      { label: "Cor da Peça", values: ["#fff", "#000", "#00f"] },
+      { label: "Tamanho da Peça", values: ["PP", "P", "M", "G", "GG"] },
+      { label: "Quantidade", inputType: "number", name: "quantidadecamiseta" },
+    ],
+    polo: [
+      {
+        label: "Material da Peça",
+        values: [
+          "Algodão Cardada",
+          "Algodão Penteada",
+          "Poliviscose - PV",
+          "Piquet",
+        ],
+      },
+      { label: "Cor da Peça", values: ["#000", "#888", "#c0c0c0"] },
+      { label: "Tamanho da Peça", values: ["PP", "P", "M", "G", "GG"] },
+      { label: "Quantidade", inputType: "number", name: "quantidadepolo" },
+    ],
+    agasalho: [
+      { label: "Malha da Peça", values: ["Moleton Flanelado"] },
+      { label: "Cor da Peça", values: ["#fff", "#808080", "#d3d3d3"] },
+      { label: "Capuz", values: ["Com Capuz", "Sem Capuz"] },
+      { label: "Bolso", values: ["Com Bolso", "Sem Bolso"] },
+      { label: "Tamanho da Peça", values: ["PP", "P", "M", "G", "GG"] },
+      { label: "Quantidade", inputType: "number", name: "quantidadeagasalho" },
+    ],
+    abada: [
+      {
+        label: "Malha da Peça",
+        values: [
+          "Algodão Cardada",
+          "Algodão Penteada",
+          "Poliviscose - PV",
+          "Dry fit Poliéster",
+          "Dry fit Poliamida",
+        ],
+      },
+      { label: "Cor da Peça", values: ["#fff", "#000", "#00f"] },
+      { label: "Abertura Lateral", values: ["Fechado", "Elastico"] },
+      { label: "Tamanho da Peça", values: ["PP", "P", "M", "G", "GG"] },
+      { label: "Quantidade", inputType: "number", name: "quantidadeabada" },
+    ],
+    ecobag: [
+      { label: "Malha da Peça", values: ["Algodão Cru"] },
+      { label: "Cor da Peça", values: ["#C7A975"] },
+      { label: "Tipo de Alça", values: ["Algodao Trançado"] },
+      { label: "Tamanho da Peça", values: ["Unico"] },
+      { label: "Quantidade", inputType: "number", name: "quantidadeecobag" },
+    ],
+    mascara: [
+      {
+        label: "Malha da Peça",
+        values: ["Algodão Cardada", "Algodão Penteada", "Poliviscose - PV"],
+      },
+      { label: "Cor da Peça", values: ["#fff", "#808080", "#d3d3d3"] },
+      { label: "Tipo de Forma", values: ["Bico de Pato"] },
+      { label: "Tamanho da Peça", values: ["Unico"] },
+      { label: "Quantidade", inputType: "number", name: "quantidademascara" },
+    ],
+  };
+
+  if (customizationOptions[item]) {
+    customizationOptions[item].forEach((option) => {
+      const { label, values, inputType } = option;
+
+      const inputElement =
+        inputType === "number"
+          ? createNumberInput(item, label)
+          : createSelectInput(item, label, values);
+
+      const inputContainer = createInputContainer(label, inputElement);
+
+      customizationOptionsContainer.appendChild(inputContainer);
+    });
   }
-  
-  // Function to close the shopping cart
-  function closeCart() {
-    document.getElementById('cart').style.display = 'none';
-  }
-  
-  // Function to save customization options to the basket
-  function saveCustomization() {
-    // Get customization options from the cart
-    const customizationOptions = document.getElementById('customization-options').innerHTML;
-  
-    // Create a list item to represent the customized product
-    const listItem = document.createElement('li');
-    listItem.innerHTML = customizationOptions;
-  
-    // Add the customized product to the basket list
-    document.getElementById('basket-list').appendChild(listItem);
-  
-    // Close the shopping cart after saving customization
-    closeCart();
-  }
-  
-  // Function to clear customization options in the shopping cart
-  function clearCustomization() {
-    // Clear customization options
-    document.getElementById('customization-options').innerHTML = '';
-  
-    // Close the shopping cart after clearing customization
-    closeCart();
-  }
-  
-  // Function to submit the form (placeholder, actual implementation is commented)
-  function submitForm() {
-    // Get the content of the basket
-    const basketContent = document.getElementById('basket-list').innerHTML;
-  
-    // Log the basket content (you can implement logic to submit the form to the server here)
-    console.log(basketContent);
-    // Implement the logic to submit the form to the server (e.g., via AJAX or traditional form submission).
-  }
+}
+
+function createNumberInput(item, label) {
+  const numberInput = document.createElement("input");
+  numberInput.type = "number";
+  numberInput.name = `${item}_${label.toLowerCase().replace(/\s+/g, "_")}`;
+  numberInput.className = "inputDetalhes required";
+
+  // Add event listener to validate input (allow only numbers)
+  numberInput.addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9]/g, "");
+  });
+
+  return numberInput;
+}
+
+function createSelectInput(item, label, values) {
+  const selectElement = document.createElement("select");
+  selectElement.name = `${item}_${label.toLowerCase().replace(/\s+/g, "_")}`;
+  selectElement.className = "inputDetalhes required";
+
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.selected = true;
+  defaultOption.innerText = "▫️ Selecionar ▫️";
+  selectElement.appendChild(defaultOption);
+
+  values.forEach((value) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = value;
+    optionElement.innerText = value;
+    selectElement.appendChild(optionElement);
+  });
+
+  return selectElement;
+}
+
+function createInputContainer(label, inputElement) {
+  const container = document.createElement("div");
+  container.classList.add("customization-option");
+
+  const labelElement = document.createElement("label");
+  labelElement.innerText = label;
+  container.appendChild(labelElement);
+
+  container.appendChild(inputElement);
+
+  return container;
+}
+
+// Function to submit the form (placeholder, actual implementation is commented)
+function submitForm() {
+  // Get the content of the basket
+  const basketContent = document.getElementById("basket-list").innerHTML;
+
+  // Log the basket content (you can implement logic to submit the form to the server here)
+  console.log(basketContent);
+  // Implement the logic to submit the form to the server (e.g., via AJAX or traditional form submission).
+}
---
+++ submit_form.php
@@ -0,0 +1,57 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Include the function for sanitizing input
+    require_once("sanitize_input.php");
+
+    // Retrieve form data
+    $nome = sanitize_input($_POST["Nome"]);
+    $email = filter_var($_POST["Email"], FILTER_VALIDATE_EMAIL);
+    $dddCel = sanitize_input($_POST["dddCel"]);
+    $telefone = sanitize_input($_POST["Telefone"]);
+    $dddTelefone = sanitize_input($_POST["dddTelefone"]);
+    $telefone2 = sanitize_input($_POST["Telefone2"]);
+    $comentario = sanitize_input($_POST["Comentario"]);
+    $basketContent = sanitize_input($_POST["basketContent"]);
+
+    // Validate email
+    if (!$email) {
+        echo "Endereço de e-mail inválido.";
+        exit();
+    }
+
+    // Parâmetros do e-mail
+    $to = "victoryagoc@gmail.com";
+    $subject = "Novo Pedido da BlueBerryCamisetas";
+
+    // Compor a mensagem de e-mail
+    $message = "Detalhes do novo pedido:\n\n";
+    $message .= "Nome: $nome\n";
+    $message .= "E-mail: $email\n";
+    $message .= "Telefone: +$dddCel $telefone\n";
+    $message .= "Telefone Secundário: +$dddTelefone $telefone2\n";
+    $message .= "Comentários: $comentario\n\n";
+    $message .= "Detalhes do Pedido:\n$basketContent";
+
+    // Cabeçalhos adicionais
+    $headers = "De: admin@blueberrycamisetas.com";
+
+    // Definir o tipo de conteúdo como texto simples
+    $headers .= "\r\nContent-Type: text/plain; charset=UTF-8";
+
+    // Definir o cabeçalho de Política de Segurança de Conteúdo (CSP)
+    header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline';");
+
+    // Enviar o e-mail
+    $success = mail($to, $subject, $message, $headers);
+
+    if ($success) {
+        echo "E-mail enviado com sucesso.";
+    } else {
+        echo "Erro ao enviar o e-mail.";
+    }
+} else {
+    // Redirecionar para a página do formulário se acessada diretamente sem submissão
+    header("Location: sua_pagina_de_formulario.html");
+    exit();
+}
+?>
